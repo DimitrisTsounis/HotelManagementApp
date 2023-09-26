47ba2099ff3e02a4137fbc02d46977625723c64a
Dimitris Tsounis <dimitris_tsounis@windowslive.com>
2023-09-26 12:04:36 +0300

    Remove ignored files

---
+++ HotelManagement/Controllers/HotelsController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HotelsController : ControllerBase
+{
+}
--- HotelManagement/Models/Hotel.cs
+++ HotelManagement/Models/Hotel.cs
@@ -5,6 +5,5 @@ public class Hotel
     public int Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
-    public int StarRating { get; set; }
-
+    public short StarRating { get; set; }
 }
