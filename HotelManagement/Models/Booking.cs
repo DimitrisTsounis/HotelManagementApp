3d993770781096d713d64278dda3153f461852c4
Dimitris Tsounis <dimitris_tsounis@windowslive.com>
2023-09-26 11:28:24 +0300

    added models and styleCop

---
+++ HotelManagement/Models/Booking.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelManagement.Models;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public int HotelId { get; set; }
+    public string CustomerName { get; set; }
+    public int NumberOfPax { get; set; }
+}
---
+++ HotelManagement/Models/Hotel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagement.Models;
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public int StarRating { get; set; }
+
+}
