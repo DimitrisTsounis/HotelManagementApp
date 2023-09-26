71e82d41f52123939a5fbf7e89e5e98b72c8e138
Dimitris Tsounis <dimitris_tsounis@windowslive.com>
2023-09-26 13:14:56 +0300

    added dbContext

---
+++ HotelManagement.Infrastructure/HotelManagementDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HotelManagement.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Infrastructure;
+
+public class HotelManagementDbContext : DbContext
+{
+    public HotelManagementDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+}
